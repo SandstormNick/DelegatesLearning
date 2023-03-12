5b1d12550b155b339c8ad1b2ac4589176172a679
SandstormNick <gibbensnick@gmail.com>
2023-03-12 22:47:35 +0200

    Added 2 example projects

---
+++ Delegates_Learning/Declaring_Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿//Declare a delegate
+delegate void Del(int i, double j);
+
+class MathClass
+{
+    static void Main()
+    {
+        MathClass m = new MathClass();
+
+        //Delegate instatiation using "MultiplyNumbers"
+        Del d = m.MultiplyNumbers;
+
+        //Invoke the delegate object.
+        Console.WriteLine("Invoking the delegate using 'MultiplyNumbers'");
+        for (int i = 1; i <= 5; i++)
+            d(i, 2);
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+
+    //Declare the associated method.
+    void MultiplyNumbers(int m, double n)
+    {
+        Console.WriteLine(m * n + " ");
+    }
+}
---
+++ Delegates_Learning/Static_And_Instance_Mapping/Program.cs
@@ -0,0 +1,31 @@
+﻿// Declare a delegate
+delegate void Del();
+
+class SampleClass
+{
+    public void InstanceMethod()
+    {
+        Console.WriteLine("A message from the instance method.");
+    }
+
+    static public void StaticMethod()
+    {
+        Console.WriteLine("A message from the static method.");
+    }
+}
+
+class TestSampleClass
+{
+    static void Main()
+    {
+        var sc = new SampleClass();
+
+        //Map the delegate to the instance method:
+        Del d = sc.InstanceMethod;
+        d();
+
+        //Map to the static method
+        d = SampleClass.StaticMethod;
+        d();
+    }
+}
